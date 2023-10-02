111694cfee69b0edeb884e368d56381b3be0224a
Sergio Guachalla <sergioguachalla0@gmail.com>
2023-10-02 16:58:27 -0400

    environment change

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  API_URL: 'http://host.docker.internal:8081/api/v1/',
-  KEYCLOAK_URL: 'http://localhost:8080',
+  API_URL: 'http://localhost:8081/api/v1/',
+  KEYCLOAK_URL: 'http://host.docker.internal:8080',
 
 };
